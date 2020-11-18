c7c91bd10bcd0609de93ca9056f9c2953dcd9f5f
me.gorkov <me.gorkov@tensor.ru>
2020-11-18 16:30:55 +0300

    fix warnings

--- utils.cc
+++ utils.cc
@@ -8,6 +8,8 @@ long get_hrtime() {
 
   if (clock_gettime(CLOCK_MONOTONIC, &hrtime) == 0) {
     return hrtime.tv_sec * NANOS_PER_SEC + hrtime.tv_nsec;
+  } else {
+    return 0;
   }
 }
 
@@ -18,5 +20,7 @@ long get_cputime(int who) {
     long user_cpu = stats.ru_utime.tv_sec * MICROS_PER_SEC + stats.ru_utime.tv_usec;
     long kernel_cpu = stats.ru_stime.tv_sec * MICROS_PER_SEC + stats.ru_stime.tv_usec;
     return user_cpu + kernel_cpu;
+  } else {
+    return 0;
   }
 }
