df16cc4fadbc4d69e9738067da87bc51a6291d25
me.gorkov <me.gorkov@tensor.ru>
2022-07-12 08:52:47 +0300

    убрал bindings - так проще собирать с pkg

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { monitorEventLoopDelay, PerformanceObserver, constants } = require('perf_
 const STATS_INTERVAL = 5;
 const RESOLUTION = 10;
 
-const { CpuLoad } = require('bindings')('sysstat');
+const { CpuLoad } = require('./build/Release/sysstat');
 const { initStats, statKeys, gcStatKeys } = require('./initstats');
 
 class Stats extends EventEmitter {
